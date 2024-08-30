52284343187d12be547510e77cf0a7ff42cd4b11
shubhamg2a50 <shubham.g2a50@gmail.com>
2024-08-30 17:27:03 +0530

    Payment system feature

--- order-tracking.js
+++
@@ -1,8 +0,0 @@
-
-const orderStatus = { 101: "Processing", 102: "Shipped", 103: "Delivered"};
-
-function trackorder(orderId) {
-    return orderStatus[orderId] || "Order not found!";
-}
-console.log(trackorder (102));
-console.log(trackorder(103));
